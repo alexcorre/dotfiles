2289a1e80086ba83b364fb1e5c913df41a8fd953
Alex Corre <aecorre@linkedin.com>
2012-08-21 16:50:12 -0700

    added paths for ios dev

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,6 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="/usr/local/bin:$HOME/Developer/devtools/utilities:$PATH"
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
