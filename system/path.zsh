4fb748264e6aad29f9a5d9d9248d6552680a7a77
Alex Corre <aecorre@linkedin.com>
2012-08-22 07:50:01 -0700

    added android tool paths

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,6 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+
+# Add iOS tools paths
 export PATH="/usr/local/bin:$HOME/Developer/devtools/utilities:$PATH"
 export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 
@@ -6,3 +8,6 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # Add NPM packages to node path
 export NODEPATH="/usr/local/lib/node_modules:$NODEPATH"
+
+# Add Android Tools paths
+export PATH="~/android-sdks/tools:~/android-sdks/platform-tools:$PATH"
