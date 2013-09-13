136a4a01da3934e852bc5247f941e129343205a3
Alex Corre <aecorre@linkedin.com>
2013-09-13 07:50:14 -0700

    fix PATH

--- rbenv/rbenv.zsh
+++ rbenv/rbenv.zsh
@@ -1,1 +1,5 @@
+# Add rbenv to beginning of path
+export PATH="$HOME/.rbenv/bin:$PATH"
+
+# Run init
 eval "$(rbenv init -)"
--- system/path.zsh
+++ system/path.zsh
@@ -4,8 +4,8 @@ export PATH="/usr/local/git/bin:$PATH"
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 # Add iOS tools paths
-export PATH="/usr/local/bin:$HOME/Developer/devtools/utilities:$PATH"
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
+export PATH="$PATH:/usr/local/bin:$HOME/Developer/devtools/utilities"
+export PATH="$PATH:/opt/local/bin:/opt/local/sbin"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
@@ -13,13 +13,10 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export NODEPATH="/usr/local/lib/node_modules:$NODEPATH"
 
 # Add Android Tools paths
-export PATH="/Users/aecorre/android-sdks/tools:/Users/aecorre/android-sdks/platform-tools:$PATH"
+export PATH="$PATH:/Users/aecorre/android-sdks/tools:/Users/aecorre/android-sdks/platform-tools"
 
 # Add linkedin commands
 export PATH="$PATH:/usr/local/linkedin/bin"
 
 # Add npm bin
 export PATH="$PATH:/usr/local/share/npm/bin"
-
-# Add rbenv to path
-export PATH="$HOME/.rbenv/bin:$PATH"
