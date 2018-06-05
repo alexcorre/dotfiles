ffed78a97519e8302638fc72a14dd82062c59a2a
Alex Corre <alex@workpop.com>
2018-06-05 08:55:02 -0700

    fix(PATH): put android / ios in beginning of path

--- system/path.zsh
+++ system/path.zsh
@@ -4,12 +4,12 @@ export PATH="/usr/local/git/bin:$PATH"
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 # Add iOS tools paths
-export PATH="$PATH:/usr/local/bin:$HOME/Developer/devtools/utilities"
-export PATH="$PATH:/opt/local/bin:/opt/local/sbin"
+export PATH="/usr/local/bin:$HOME/Developer/devtools/utilities:$PATH"
+export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
 
 # Android
-export PATH="$PATH:/Users/alexcorre/Library/Android/sdk/tools"
-export PATH="$PATH:/Users/alexcorre/Library/Android/sdk/platform-tools"
+export PATH="/Users/alexcorre/Library/Android/sdk/tools:$PATH"
+export PATH="/Users/alexcorre/Library/Android/sdk/platform-tools:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
